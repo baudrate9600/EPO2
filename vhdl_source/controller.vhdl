f7d0befc9b2241a2d7be9f0b8bb4cf7b5e452b60
Sauce <33604943+baudrate9600@users.noreply.github.com>
2020-06-04 11:21:43 +0200

    Update controller.vhdl

--- vhdl_source/controller.vhdl
+++ vhdl_source/controller.vhdl
@@ -1,172 +1,138 @@
 library IEEE;
+-- Hier komen de gebruikte libraries:
+use IEEE.numeric_std.all;
 use IEEE.std_logic_1164.all;
-use ieee.numeric_std.all;
 
 entity controller is
-	port (	clk			: in	std_logic;
-		reset			: in	std_logic;
+port ( clk : in std_logic;
+reset : in std_logic;
 
-		sensor_l		: in	std_logic;
-		sensor_m		: in	std_logic;
-		sensor_r		: in	std_logic;
-		mine_detect		: in	std_logic;
+sensor_l : in std_logic;
+sensor_m : in std_logic;
+sensor_r : in std_logic;
+mine_detect: in std_logic;
+count_in : in std_logic_vector (19 downto 0);
+count_reset : out std_logic;
 
-		count_in		: in	std_logic_vector (19 downto 0);
-		count_reset		: out	std_logic;
+motor_l_reset : out std_logic;
+motor_l_direction : out std_logic;
 
-		motor_l_reset		: out	std_logic;
-		motor_l_direction	: out	std_logic;
-
-		motor_r_reset		: out	std_logic;
-		motor_r_direction	: out	std_logic
-	);
+motor_r_reset : out std_logic;
+motor_r_direction : out std_logic
+);
 end entity controller;
 
-architecture behaviour of controller is
-
-	type controller_state is ( 	motor_uit ,
-					motor_both,
-					motor_gentleleft,
-					motor_gentleright,
-					motor_sharpleft,
-					motor_sharpright,
-					motor_white_mine,
-					motor_right_mine,
-					motor_reset_mine);
-	signal state, new_state : controller_state;
-
+architecture controller_behav of controller is
+type diff_states is (Startturn,Sensor_check,Wait_for_line);
+signal state, next_state: diff_states;
+signal sensor: std_logic_vector(2 downto 0); 
+signal mine : std_logic; -- using for being in state mine_detect.
+signal motorreset: std_logic;
+signal reset_l_motor, reset_r_motor: std_logic;
 begin
-	process ( clk )
-	begin
-		if (clk'event and clk = '1') then
-			if (reset = '1') then
-				state <= motor_uit; 
-			else
-				state <= new_state;
-			end if ;
-		end if ;
-	end process ;
+sensor(2)<=sensor_l;
+sensor(1)<=sensor_m;
+sensor(0)<=sensor_r;
+ttl:process(sensor,state,mine_detect)
+  begin
+case state is
+when Startturn =>
+motor_l_direction <= '1';
+motor_r_direction <= '1';
+reset_l_motor <= '0';
+reset_r_motor <= '0';
+if(sensor="111") then
+next_state <= Wait_for_line;
+else next_state<=Startturn;
+end if;
+when Wait_for_line =>
+motor_l_direction <= '1';
+motor_r_direction <= '1';
+if(sensor="110") then
+next_state <= Sensor_check;
+else next_state <= Wait_for_line;
+end if;
+when Sensor_check=>
+if (sensor="000") then
+motor_l_direction <= '1';
+        motor_r_direction <= '0';
+reset_l_motor <= '0';
+reset_r_motor <= '0';
 
-	process (state, count_in, sensor_l, sensor_r, sensor_m, mine_detect)
-	begin
-		case state is
-			when motor_uit =>
-				count_reset <= '1';
-				motor_l_reset <= '1';
-				motor_r_reset <= '1';
-				if (mine_detect = '1') then
-					new_state <=	motor_reset_mine;
-				else
-					if (sensor_l = '0' and sensor_m = '0' and sensor_r = '0') then
-						new_state <= 	motor_both; 
-					elsif (sensor_l = '0' and sensor_m = '0' and sensor_r = '1') then
-						new_state <= 	motor_gentleleft;
-					elsif (sensor_l = '0' and sensor_m = '1' and sensor_r = '0') then
-						new_state <= 	motor_both;
-					elsif (sensor_l = '0' and sensor_m = '1' and sensor_r = '1') then
-						new_state <= 	motor_sharpleft;
-					elsif (sensor_l = '1' and sensor_m = '0' and sensor_r = '0') then
-						new_state <= 	motor_gentleright;
-					elsif (sensor_l = '1' and sensor_m = '0' and sensor_r = '1') then
-						new_state <= 	motor_both; 	
-					elsif (sensor_l = '1' and sensor_m = '1' and sensor_r = '0') then
-						new_state <= 	motor_sharpright;
-					elsif (sensor_l = '1' and sensor_m = '1' and sensor_r = '1') then
-						new_state <= 	motor_both; 	
-					else
-						new_state <= 	motor_uit; 
-					end if;
-				end if;
-			when motor_white_mine =>
-				count_reset <= '0';
-				motor_l_reset <= '0';
-				motor_r_reset <= '0';
-				motor_l_direction <= '1';		
-				motor_r_direction <= '1';
-				if (unsigned(count_in) = 1000000 and sensor_l = '1' and sensor_m = '1' and sensor_r = '0') then
-					new_state <= 	motor_uit; 
-				elsif (unsigned(count_in) = 1000000) then
-					new_state <=	motor_reset_mine;
-				else
-					new_state <= 	motor_white_mine;
-				end if;
-			when motor_right_mine =>
-				count_reset <= '0';
-				motor_l_reset <= '0';
-				motor_r_reset <= '0';
-				motor_l_direction <= '1';		
-				motor_r_direction <= '1';
-				if (unsigned(count_in) = 1000000) then
-					new_state <=	motor_reset_mine;
-				else
-					new_state <= 	motor_right_mine;
-				end if;
-			when motor_reset_mine =>
-				count_reset <= '1';
-				motor_l_reset <= '1';
-				motor_r_reset <= '1';
-				motor_l_direction <= '1';		
-				motor_r_direction <= '1';
-				if (sensor_l = '1' and sensor_m = '1' and sensor_r = '1') then
-					new_state <=	motor_white_mine;
-				else
-					new_state <=	motor_right_mine;
-				end if;
-			when motor_both =>
-				count_reset <= '0';
-				motor_l_reset <= '0';
-				motor_r_reset <= '0';
-				motor_l_direction <= '1';		
-				motor_r_direction <= '0';	
-				if (unsigned(count_in) = 1000000) then
-					new_state <= motor_uit;
-				else
-					new_state <= motor_both;
-				end if;
-			when motor_gentleleft =>
-				count_reset <= '0';
-				motor_l_reset <= '1';
-				motor_r_reset <= '0';
-				motor_l_direction <= '0';		
-				motor_r_direction <= '0';	
-				if (unsigned(count_in) = 1000000) then
-					new_state <= motor_uit;
-				else
-					new_state <= motor_gentleleft;
-				end if;		
-			when motor_sharpleft =>
-				count_reset <= '0';
-				motor_l_reset <= '0';
-				motor_r_reset <= '0';
-				motor_l_direction <= '0';		
-				motor_r_direction <= '0';	
-				if (unsigned(count_in) = 1000000) then
-					new_state <= motor_uit;
-				else
-					new_state <= motor_sharpleft;
-				end if;
-			when motor_gentleright =>
-				count_reset <= '0';
-				motor_l_reset <= '0';
-				motor_r_reset <= '1';
-				motor_l_direction <= '1';		
-				motor_r_direction <= '0';	
-				if (unsigned(count_in) = 1000000) then
-					new_state <= motor_uit;
-				else
-					new_state <= motor_gentleright;
-				end if;	
-			when motor_sharpright =>
-				count_reset <= '0';
-				motor_l_reset <= '0';
-				motor_r_reset <= '0';
-				motor_l_direction <= '1';		
-				motor_r_direction <= '1';	
-				if (unsigned(count_in) = 1000000) then
-					new_state <= motor_uit;
-				else
-					new_state <= motor_sharpright;
-				end if;
-		end case;
-	end process;
-end architecture behaviour;
+elsif(sensor= "001") then
+        motor_l_direction <= '0';
+        motor_r_direction <= '0';
+reset_l_motor <= '1';
+reset_r_motor <= '0';
+
+elsif(sensor= "010") then
+motor_l_direction <= '1';
+        motor_r_direction <= '0';
+reset_l_motor <= '0';
+reset_r_motor <= '0';
+
+elsif(sensor= "011") then
+        motor_l_direction <= '0';
+        motor_r_direction <= '0';
+reset_l_motor <= '0';
+reset_r_motor <= '0';
+
+elsif(sensor= "100") then
+motor_l_direction <= '1';
+        motor_r_direction <= '0';
+reset_r_motor <= '1';
+reset_l_motor <= '0';
+
+elsif(sensor= "101") then
+motor_l_direction <= '1';
+        motor_r_direction <= '0';
+reset_l_motor <= '0';
+reset_r_motor <= '0';
+
+elsif(sensor= "110") then
+motor_l_direction <= '1';
+        motor_r_direction <= '1';
+reset_l_motor <= '0';
+reset_r_motor <= '0';
+
+elsif(sensor= "111") then
+motor_l_direction <= '1';
+        motor_r_direction <= '0';
+reset_l_motor <= '0';
+reset_r_motor <= '0';
+
+else
+motor_l_direction <= '0';
+        motor_r_direction <= '0';
+reset_l_motor <= '0';
+reset_r_motor <= '0';
+        end if;
+if(mine_detect='1') then
+next_state<=Startturn;
+else
+next_state<=Sensor_check;
+end if;
+     end case;       
+end process;
+clk_sig: process(clk,reset)
+begin
+if (reset='1') then
+    count_reset <= '1';
+    motorreset <= '1';
+    state<=Sensor_check;
+    elsif (clk'event and clk='1') then
+state<=next_state;
+        if (unsigned(count_in) =1000000) then
+      count_reset <= '1';
+      motorreset <= '1';
+    else
+      count_reset <= '0';
+      motorreset <= '0';
+    end if;
+end if;
+end process;
+
+motor_l_reset <= reset_l_motor or motorreset;
+motor_r_reset <= reset_r_motor or motorreset;
+
+end controller_behav;
