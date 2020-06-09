7e0b343102dfa298075858dcb9702ea1769c0058
Olasoji <olasojimakinwa@gmail.com>
2020-06-09 11:41:01 +0200

    refactor

--- vhdl_source/mine_freq.vhdl
+++ vhdl_source/mine_freq.vhdl
@@ -14,9 +14,9 @@ end entity mine_freq;
 architecture behaviour of mine_freq is
 
 	type mine_state is ( 	
-				reset_state,
-				wait_for_high, 
-				count_high
+				S0,
+				S1, 
+				S2
 				);
 	signal state, new_state : mine_state;
 	signal mine_count, new_mine_count	: unsigned(19 downto 0);
@@ -26,7 +26,7 @@ begin
 	begin
 		if reset = '1' then 
 		
-			state <= reset_state; 
+			state <= S0; 
 		 
 		elsif rising_edge(clk) then 
 			state <= new_state;	
@@ -37,24 +37,24 @@ begin
 	process (state, mine_sensor,new_mine_count)
 	begin
 		case state is
-			when reset_state => 
+			when S0 => 
 				mine_count <= (others => '0');
 				mine_detect <= '0';
-				new_state <= wait_for_high;
-			when wait_for_high =>
+				new_state <= S1;
+			when S1 =>
 				if mine_sensor = '1' then 
 					mine_count <= (others => '0');
-					new_state <= count_high; 
+					new_state <= S2; 
 				end if; 
-			when count_high => 
+			when S2 => 
 				mine_count <= new_mine_count;
 				if mine_sensor = '0' then 
-					if (unsigned(mine_count) < 2500) then 
+					if (unsigned(mine_count) < 2777) then 
 						mine_detect <= '0'; 
 					else  
 						mine_detect <= '1'; 
 					end if; 
-					new_state <= wait_for_high;
+					new_state <= S1;
 				end if; 
 		end case;
 	end process;
